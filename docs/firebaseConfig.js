06a772873da474ed91a604ab2905092209bec762
大政信博 <oomasanobuhiro@oomasanobuhironoMacBook-Air.local>
2024-10-13 03:32:29 +0900

    GitHub Pagesに反映するものたちを追加

--- docs/firebaseConfig.js
+++ docs/firebaseConfig.js
@@ -4,28 +4,32 @@
 
 // firebaseConfig.js
 
-// Firebase SDKをインポート
+// firebaseConfig.js
+
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
 
-// Firebase の設定
+// Firebaseの設定情報
 const firebaseConfig = {
     apiKey: "AIzaSyBiHjBk7rMiz4Fw7IoLPpFo1FTrQxqy_58",
     authDomain: "acau-3ebcf.firebaseapp.com",
     projectId: "acau-3ebcf",
     storageBucket: "acau-3ebcf.appspot.com",
     messagingSenderId: "407904196633",
-    appId: "your-app-id-here" // もし必要ならappIdを追加
+    appId: "your-app-id"
 };
 
-// Firebaseアプリを初期化
+// Firebaseアプリの初期化
 const app = initializeApp(firebaseConfig);
 
-// Firestoreのインスタンスを作成
+// Firebaseサービスの初期化
+const auth = getAuth(app);
 const db = getFirestore(app);
 
-// 他のファイルからインポートできるようにエクスポート
-export { db };
+// 他のファイルから利用できるようにエクスポート
+export { auth, db };
+
 
 
 
